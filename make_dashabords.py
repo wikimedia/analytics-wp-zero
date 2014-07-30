7ee3ccdedacf05b21c16c71eed0bf846e3e81003
Christian Aistleitner <christian@quelltextlich.at>
2014-07-30 11:52:56 +0000

    Mark 2014-07-08, and 2014-07-13 as bad dates
    Due to soccer events, traffic spiked for those two days, which
    overloaded our logging infrastructure, and caused packetloss.
    Hence, we do not trust data for those days.
    
    Bug: 67694

--- make_dashabords.py
+++ make_dashabords.py
@@ -113,6 +113,8 @@ BAD_DATES = [
                 datetime.date(2013,8,18),
                 datetime.date(2014,1,5),
                 datetime.date(2014,1,6),
+                datetime.date(2014,7,8),
+                datetime.date(2014,7,13),
             ]
 
 
